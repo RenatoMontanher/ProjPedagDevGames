ce7f1f2744231b8d8fe720f081e7a1c4093e0e50
renato-montanha <renatomontanher.rm@gmail.com>
2018-09-04 21:08:11 -0300

    Fim aula 02

--- Button.gd
+++ Button.gd
@@ -3,23 +3,10 @@ extends Button
 var resultado = 0
 
 func _ready():
-	somar("o numero é ", 10)
 	print("objeto criado!");
-	"""label = get_node(caminhoLabel)"""
 	pass
 
 func _on_Button_pressed():
-	"""
-	resultado = resultado + 2
-	print("o resultado final é: ",resultado);
-	label.text = str("o resultado é ",resultado)
-	"""
-	var lineEdit = get_node("../LineEdit");
-	var label1 = get_node("../Label")
-	resultado = resultado + int(lineEdit.text)
-	label1.text=str(resultado)	
+	resultado = resultado+2
+	print("o Resultado Final é: ", resultado);
 	pass 
-	
-func somar (palavra, numero):
-	print(palavra, numero)
-	pass
