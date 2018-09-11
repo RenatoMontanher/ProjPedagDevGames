f83eb73386d92eea079b8c50f41347fae65b4d24
renato-montanha <renatomontanher.rm@gmail.com>
2018-09-11 18:50:15 -0300

    organizando projeto

---
+++ TesteButton.gd
@@ -0,0 +1,22 @@
+extends Button
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	
+	var resultado1 = 1 + 9
+	var resultado2 = 9 + 2
+	var resultado3 = 4 + 2
+	var resultado4 = 5 + 3
+	var resultado5 = 3 + 5
+	var resultado6 = 2 + 4
+	
+	
+	""" printar resultados no console ....."""
+	pass
+	
+func somar(numero1, numero2):
+	return numero1 + numero2
+
