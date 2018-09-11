7ed11114b8b4ebd8b7c819603c959f1b879e15df
renato-montanha <renatomontanher.rm@gmail.com>
2018-09-11 19:18:07 -0300

    Resolução exercicio 2!!!

---
+++ scripts/btnSomar.gd
@@ -0,0 +1,20 @@
+extends Button
+
+var lineEditN1 
+var lineEditN2
+var labelResultado
+
+func _ready():
+	lineEditN1 = get_node("../HBoxContainer/VBoxContainer/LineEdit1")
+	lineEditN2 = get_node("../HBoxContainer/VBoxContainer2/LineEdit2")
+	labelResultado = get_node("../labelResultado")
+	print(_somar(3,3))
+	pass
+
+func _somar(num1,num2):
+	return num1+num2
+
+
+func _on_btnSomar_pressed():
+	labelResultado.text = str(_somar(int(lineEditN1.text),int(lineEditN2.text)))
+	pass # replace with function body
